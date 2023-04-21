6c4d6181c6225fb5a099204406dc14da01d09c68
ACupOfWhiskey <mia.nishida@gmail.com>
2023-04-21 20:20:45 +0300

    complete 2nd task

---
+++ 2nd_lesson/task_calculator.java
@@ -0,0 +1,40 @@
+package 2nd_lesson;
+
+import java.util.Scanner;
+
+public class task_calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Choose an operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Operation selected incorrectly");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
