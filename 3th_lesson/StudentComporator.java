1e64ec4c987c4136cc985ed35c55c06c912e4d0b
ACupOfWhiskey <mia.nishida@gmail.com>
2023-04-25 19:58:03 +0300

    complete 3th lesson

---
+++ 3th_lesson/StudentComporator.java
@@ -0,0 +1,19 @@
+package 3th_lesson;
+
+import java.util.Comparator;
+
+public class StudentComporator implements Comparator<Student> {
+
+    public int compare(Student student, Student anotherStudent) {
+        if (student.getAverage() > anotherStudent.getAverage())
+            return -1;
+        if (student.getAverage() < anotherStudent.getAverage())
+            return 1;
+        if (student.getLastName().length() > anotherStudent.getLastName().length())
+            return -1;
+        if (student.getLastName().length() < anotherStudent.getLastName().length())
+            return 1;
+        return 0;
+    }
+   
+}
---
+++ 3th_lesson/TeacherComparator.java
@@ -0,0 +1,17 @@
+package 3th_lesson;
+
+import java.util.Comparator;
+
+public class TeacherComparator implements Comparator<Teacher> {
+
+    @Override
+    public int compare(Teacher o1, Teacher o2) {
+        if (o1.getId() > o2.getId()) {
+            return 1;
+        } else if (o1.getId() < o2.getId()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
